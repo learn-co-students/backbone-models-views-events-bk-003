8080f0cd025a699d8df3229d49d9b205697bfe89
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-09-03 15:42:05 -0400

    fix spelling in spec.

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe( "#boostrap", function() {
+describe( "#bootstrap", function() {
   it("should instantiate your view and return the view object", function() {
     bootstrap();
     expect($("body")).toContainHtml('<form><label for="name"></label><input id="name" type="text" name="name"><input id= "mySubmit" type="submit" value="submit"></form>');
