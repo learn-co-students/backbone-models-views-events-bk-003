810bc0ce8dcba5d02f6c1c45230472b3e7808fdc
JaimieWalker <jaimie.walker90@gmail.com>
2015-11-23 16:08:47 -0500

    Learned some new tricks

--- views/main.js
+++ views/main.js
@@ -1,27 +1,30 @@
-MyView = Backbone.View.extend({
+FormView = Backbone.View.extend({
   events : {
     // listen to the submission of forms here
     // when a from is sumbitted, call on the the submitHandler function
+    "submit form" : "submitHandler"
   },
   initialize: function() {
     // call on the render function here
+    this.render();
 
   },
   render: function() {
     // the html you'll be working with is 
-    // <form><label for="name">
-    //  </label><input id="name" type="text" name="name">
-    //  <input id= "mySubmit" type="submit" value="submit">
-    // </form>
+    html = '<form><label for="name"></label><input id="name" type="text" name="name"><input id= "mySubmit" type="submit" value="submit"></form>'
 
+    this.$el.append(html)
+
+    return this;
     // remember, it's typical to return the instance of this class here
   },
-  submitHandler: function() {
+  submitHandler: function(event) {
     // this function should accept one parameter, the event
-
+      event.preventDefault();
     // prevent the default behavior of the form here (hint: you'll be calling a function on the parameter)
-
     // save the text value of the input field with an id of name here
+      var name = $("#name").val();
+      this.model.set("name",name);
 
     // set the model's name property to be the variable you saved above here
   }
